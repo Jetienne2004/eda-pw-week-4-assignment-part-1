7661cea8717d401164403b453823364b66eb9be8
Jaylyn Etienne <jaylynetienne3@gmail.com>
2023-12-07 21:04:22 -0600

    question 6 fixed

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -19,7 +19,7 @@ function helloName(name) {
   return `Hello, ${name}!`;
 }
 // Remember to call the function to test
-console.log(helloName());
+console.log(helloName('Jo'));
 
 // 3. Function to add two numbers together & return the result
 
@@ -57,13 +57,16 @@ console.log(isPositive(3));
 // 6. Function to return the _last_ item in an array. If the
 //    array is empty, return `undefined`.
 function getLast(array) {
+  console.log(array);
   if (array.length > 0) {
     return array[array.length - 1];
   } else {
     return undefined;
   }
 }
-
+let list = ['A','B', 'C']
+let result = getLast(list);
+console.log(result);
 // 7. Function to find a value in an array. Return true if the
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find
